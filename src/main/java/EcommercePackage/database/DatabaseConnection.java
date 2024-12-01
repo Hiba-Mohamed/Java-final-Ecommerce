a276df38ace7ffa5c1df71ef2e9fa084ad8ce5d7
Hiba-Mohamed <hibamoha987@gmail.com>
2024-12-01 16:13:13 -0330

    connecting to the database

--- src/main/java/EcommercePackage/database/DatabaseConnection.java
+++ src/main/java/EcommercePackage/database/DatabaseConnection.java
@@ -3,3 +3,19 @@ package EcommercePackage.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class DatabaseConnection {
+
+    public static Connection getConnection() throws SQLException, SQLException {
+        Dotenv dotenv = Dotenv.load();
+
+        // Get the environment variables
+        String url = dotenv.get("DB_URL");
+        String user = dotenv.get("DB_USER");
+        String password = dotenv.get("DB_PASSWORD");
+        return DriverManager.getConnection(url, user, password);
+    }
+
+
+}
