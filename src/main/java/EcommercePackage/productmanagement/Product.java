7d1f2a7061ea971a3bebdf29b9ebec797a1915ac
SummayaSiddiqui <summaya.siddiqui@keyin.com>
2024-12-04 15:59:39 -0330

    Updted the names of attributes

--- src/main/java/EcommercePackage/productmanagement/Product.java
+++ src/main/java/EcommercePackage/productmanagement/Product.java
@@ -3,18 +3,25 @@ package EcommercePackage.productmanagement;
 public class Product {
   // Attributes
     private int product_id;
-    private String name;
-    private double price;
-    private int quantity;
-    private int sellerId;
+    private String productName;
+    private double productPrice;
+    private int productQuantity;
+    private int productSellerId;
 
     // Constructor
-    public Product(int product_id, String name, double price, int quantity, int sellerId) {
+    public Product(int product_id, String productName, double productPrice, int productQuantity, int productSellerId) {
         this.product_id = product_id;
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
-        this.sellerId = sellerId;
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productQuantity = productQuantity;
+        this.productSellerId = productSellerId;
+    }
+
+    public Product(String productName, double productPrice, int productQuantity, int productSellerId) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productQuantity = productQuantity;
+        this.productSellerId = productSellerId;
     }
 
     // Getters and Setters
@@ -27,45 +34,45 @@ public class Product {
     }
 
     public String getName() {
-        return this.name;
+        return this.productName;
     }
 
-    public void setName(String product_name) {
-        this.name = product_name;
+    public void setName(String productName) {
+        this.productName = productName;
     }
 
     public double getPrice() {
-        return this.price;
+        return this.productPrice;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
+    public void setPrice(double productPrice) {
+        this.productPrice = productPrice;
     }
 
     public int getQuantity() {
-        return this.quantity;
+        return this.productQuantity;
     }
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
+    public void setQuantity(int productQuantity) {
+        this.productQuantity = productQuantity;
     }
 
     public int getSellerId() {
-        return this.sellerId;
+        return this.productSellerId;
     }
 
-    public void setSellerId(int sellerId) {
-        this.sellerId = sellerId;
+    public void setSellerId(int productSellerId) {
+        this.productSellerId = productSellerId;
     }
 
     // Utility methods
     public void updateStock(int newQuantity) {
-        this.quantity = newQuantity;
+        this.productQuantity = newQuantity;
     }
 
     @Override
     public String toString() {
-        return "Product [ID=" + this.product_id + ", Name=" + this.name + ", Price=" + this.price +
-               ", Quantity=" + this.quantity + ", Seller ID=" + this.sellerId + "CREATED AT=" + "]";
+        return "Product [ID=" + this.product_id + ", Name=" + this.productName + ", Price=" + this.productPrice +
+               ", Quantity=" + this.productQuantity + ", Seller ID=" + this.productSellerId + "]";
     }
 }
